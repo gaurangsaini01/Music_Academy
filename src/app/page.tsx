dfdfe991fffafb63ed0fdeb1aeabe9da99ecdbc4
gaurangsaini01 <gaurangsaini01@gmail.com>
2024-06-04 16:04:55 +0530

    Testimonials and WhyChooseUs section Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Testimonials from "@/components/Testimonials";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection/>
       <FeaturedCourses/>
+      <WhyChooseUs/>
+      <Testimonials/>
     </main>
   );
 }
--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import { BackgroundGradient } from "./ui/background-gradient";
-import Image from "next/image";
+import { Button } from "./ui/moving-border";
 import courseData from "../data/music_courses.json";
 
 interface Course {
@@ -16,7 +16,9 @@ interface Course {
 }
 
 const FeaturedCourses = () => {
- let featuredCourses = courseData.courses.filter((course: Course) => course.isFeatured);
+  let featuredCourses = courseData.courses.filter(
+    (course: Course) => course.isFeatured
+  );
 
   return (
     <div className="py-12 bg-gray-900">
@@ -30,23 +32,26 @@ const FeaturedCourses = () => {
       </div>
       <div className="mt-10 px-10 md:px-0">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
-            {featuredCourses.map((course:Course)=>(
-                <div key={course.id} className="flex justify-center">
-                    <BackgroundGradient
-                        className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
-                            <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
-                                <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
-                                <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
-                                <Link href={`/courses/${course.slug}`}>
-                                Learn More
-                                </Link>
-                            </div>
-                        </BackgroundGradient>
+          {featuredCourses.map((course: Course) => (
+            <div key={course.id} className="flex justify-center">
+              <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                  <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                    {course.title}
+                  </p>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                    {course.description}
+                  </p>
+                  <Link href={`/courses/${course.slug}`}>Learn More</Link>
                 </div>
-            ))}
+              </BackgroundGradient>
+            </div>
+          ))}
         </div>
       </div>
-      <div></div>
+      <div className="w-full flex items-center justify-center mt-10">
+        <div><Button className="bg-black/[0.9]">View All Courses</Button></div>
+      </div>
     </div>
   );
 };
